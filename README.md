b83171b1e22e959c6857ff6ca8890a83b660daa1
Cosmicode <anton.harbers23@gmail.com>
2025-07-20 22:44:27 +0200

    Initial Commit: React, Typescript, Vite, Tailwind

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + TypeScript + Vite + Tailwind
+
+Browser based game of Tak. Work in progress.
+
+Open Source License.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className=' text-6xl text-blue-300'>
+      Hello World!
+    </div>
+  )
+}
+
+export default App
